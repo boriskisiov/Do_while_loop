65e0bfe330721f80ad25e5091c9de40e28dc7617
boriskisiov <boriskisiov@gmail.com>
2023-10-24 13:45:42 +0300

    Add project files.

---
+++ Do_while_loop/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Do_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 10);
+        }
+    }
+}
